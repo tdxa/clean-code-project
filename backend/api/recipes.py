34fd77268b8841ca3ccb2bbfd6c66d6fd0db85b3
Patryk Dragon <patryk.dragon@outlook.com>
2023-01-24 00:48:42 +0100

    Include sample api endpoints for recipes

---
+++ backend/api/recipes.py
@@ -0,0 +1,65 @@
+from fastapi import APIRouter
+from crud.recipe_crud import RecipeService
+from models.recipe_model import RecipeResponse, Recipe, PyObjectId
+from exceptions import RecipeNotFoundException
+
+recipe_service = RecipeService()
+recipes_router = APIRouter()
+
+
+@recipes_router.post("/recipes", status_code=201)
+async def add_recipe(recipe: Recipe) -> RecipeResponse:
+    return recipe_service.insert_one(recipe)
+
+
+@recipes_router.get("/recipes/random")
+async def get_random_recipe() -> RecipeResponse:
+    return recipe_service.get_random_recipe()
+
+
+@recipes_router.get("/recipes/{id}")
+async def get_recipe_by_id(id: PyObjectId) -> RecipeResponse:
+    res = recipe_service.get_one(id)
+    if res is None:
+        raise RecipeNotFoundException(f"Recipe with id {id} not found")
+
+    return res
+
+
+@recipes_router.get("/recipes")
+async def get_all_recipes() -> list[RecipeResponse]:
+    return recipe_service.get_all()
+
+
+@recipes_router.get("/recipes/name/{name}")
+async def get_recipe_by_name(name: str) -> RecipeResponse:
+    res = recipe_service.get_one_by_name(name)
+    if not res:
+        raise RecipeNotFoundException(f"Recipe with name {name} not found")
+
+    return res
+
+
+@recipes_router.get("/recipes-names")
+async def get_all_recipes_names() -> list[str]:
+    return recipe_service.get_all_names()
+
+
+@recipes_router.get("/recipes-tags")
+async def get_all_recipes_tags():
+    return recipe_service.get_all_tags()
+
+
+@recipes_router.get("/recipes/tag/{tag}")
+async def get_all_recipes_by_tag(tag: str):
+    return recipe_service.get_all_by_tag(tag)
+
+
+@recipes_router.patch("/recipes/{id}")
+async def patch_recipe(id: PyObjectId, data: Recipe):
+    return recipe_service.update_one(id, data)
+
+
+@recipes_router.delete("/recipes/{id}", status_code=204)
+async def delete_recipy_by_id(id: PyObjectId):
+    recipe_service.delete_one_by_id(id)
