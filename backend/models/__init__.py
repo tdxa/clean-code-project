3645748ef7c58083b34c62c2d072f546ecfc53ee
daroG <darek.kce@gmail.com>
2023-01-24 21:24:18 +0100

    Fix flake8 bugs

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,7 +1,7 @@
-from .token_model import Token, TokenData
-from .user_model import User, UserCreate, UserInDB
 from .nutritional_values import NutritionalValues
 from .recipe import Recipe
+from .token_model import Token, TokenData
+from .user_model import User, UserCreate, UserInDB
 
 
 __all__ = [
