b3c1e68c950c0cf516678b00aea95bd3509bb19b
tdxa <anndoman3@gmail.com>
2023-01-28 22:24:29 +0100

    Create basic login modal with form

---
+++ frontend/src/components/Authentication/Login/index.tsx
@@ -0,0 +1,94 @@
+import {
+  Box,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+  InputAdornment,
+  TextField,
+} from '@mui/material';
+import { Controller, useForm } from 'react-hook-form';
+import React, { FC, useState } from 'react';
+import PrimaryButton from '../../Common/Buttons/PrimaryButton';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+
+interface Props {
+  open: boolean;
+  handleClose: () => void;
+}
+interface LoginData {
+  email: string;
+  password: string;
+}
+
+const LoginModal: FC<Props> = ({ open, handleClose }) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+  };
+
+  const onSubmit = (data: LoginData) => console.log(data);
+
+  return (
+    <Dialog fullWidth maxWidth='lg' open={open} onClose={handleClose}>
+      <DialogTitle>Zaloguj się</DialogTitle>
+      <DialogContent>
+        <Box>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Controller
+              name='email'
+              control={control}
+              render={({ field }) => (
+                <TextField type='email' label='E-mail' {...field} />
+              )}
+            />
+            <Controller
+              name='password'
+              control={control}
+              render={({ field }) => (
+                <TextField
+                  type={showPassword ? 'text' : 'password'}
+                  label='hasło'
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position='end'>
+                        <IconButton
+                          aria-label='toggle password visibility'
+                          onClick={handleClickShowPassword}
+                          onMouseDown={handleMouseDownPassword}
+                          edge='end'
+                        >
+                          {showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
+                  {...field}
+                />
+              )}
+            />
+            <PrimaryButton text='Zaloguj' isSubmit />
+          </form>
+        </Box>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default LoginModal;
--- frontend/src/components/Layout/Topbar/index.tsx
+++ frontend/src/components/Layout/Topbar/index.tsx
@@ -1,11 +1,12 @@
-import React, { FC } from 'react';
-import { IconButton, MenuItem } from '@mui/material';
 import * as styles from '../layout.module.scss';
+import { IconButton, MenuItem } from '@mui/material';
+import React, { FC, useState } from 'react';
+import LoginModal from '../../Authentication/Login';
 import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
-import { useIsMobile } from '../../../utils';
-import TertiaryButton from '../../Common/Buttons/TertiaryButton';
 import PrimaryButton from '../../Common/Buttons/PrimaryButton';
+import TertiaryButton from '../../Common/Buttons/TertiaryButton';
 import { muiStylesLayout } from '../muiStylesLayout';
+import { useIsMobile } from '../../../utils';
 
 interface Props {
   handleDrawerToggle?: () => void;
@@ -13,30 +14,34 @@ interface Props {
 
 const Topbar: FC<Props> = ({ handleDrawerToggle }) => {
   const isMobile = useIsMobile();
+  const [openLogin, setOpenLogin] = useState(false);
 
   return (
-    <div className={styles.topbarMain}>
-      {isMobile ? (
-        <div>
-          <IconButton
-            onClick={handleDrawerToggle}
-            sx={muiStylesLayout.iconMenu}
-          >
-            <MenuRoundedIcon />
-          </IconButton>
-        </div>
-      ) : (
-        <>
-          <MenuItem>Home</MenuItem>
-          <MenuItem>About</MenuItem>
-          <MenuItem>How it works</MenuItem>
-          <div className={styles.topbarButtons}>
-            <TertiaryButton text='Log in' />
-            <PrimaryButton text='Sign up' />
+    <>
+      <LoginModal open={openLogin} handleClose={() => setOpenLogin(false)} />
+      <div className={styles.topbarMain}>
+        {isMobile ? (
+          <div>
+            <IconButton
+              onClick={handleDrawerToggle}
+              sx={muiStylesLayout.iconMenu}
+            >
+              <MenuRoundedIcon />
+            </IconButton>
           </div>
-        </>
-      )}
-    </div>
+        ) : (
+          <>
+            <MenuItem>Home</MenuItem>
+            <MenuItem>About</MenuItem>
+            <MenuItem>How it works</MenuItem>
+            <div className={styles.topbarButtons}>
+              <TertiaryButton text='Log in' event={() => setOpenLogin(true)} />
+              <PrimaryButton text='Sign up' />
+            </div>
+          </>
+        )}
+      </div>
+    </>
   );
 };
 
